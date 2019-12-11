d715c5d0ad3326a0796074f68eea818cb232dbd5
vmlankub <someone@example.com>
2019-12-11 13:21:48 +0800

    @MinecraftFuns: Original file: C:/$Recycle.Bin/S-1-5-21-1904583442-1439502376-3578197732-500/$R23TSKK/admin/banner.inc.php

---
+++ nVxK4I3obBaSr6TZ/bb1e4a5a6afff0dab4a918e50cdf63ef860064e0.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+#	Project: PHPDISK File Storage Solution
+#	This is NOT a freeware, use is subject to license terms.
+#
+#	Site: http://www.phpdisk.com
+#
+#	$Id: banner.inc.php 25 2014-01-10 03:13:43Z along $
+#
+#	Copyright (C) 2008-2014 PHPDisk Team. All Rights Reserved.
+#
+*/
+
+include "../includes/commons.inc.php";
+
+require_once template_echo('banner',$admin_tpl_dir,'',1);
+
+include PHPDISK_ROOT."./includes/footer.inc.php";
+
+?>
